cc3648b5d118eaf7cf8adc3647c83c891f8d9b56
andii04 <and.woerrlein.17@lehre.mosbach.dhbw.de>
2019-02-27 11:22:50 +0100

    hidsah

--- src/Blue.java
+++ src/Blue.java
@@ -1,5 +1,6 @@
 public class Blue implements IState {
     private final Passenger passenger;
+    private IStateListener listener;
 
     public Blue(Passenger passenger) {
         this.passenger = passenger;
@@ -11,11 +12,12 @@ public class Blue implements IState {
         System.out.println("ConcreteState0");
         //Zustandswechsel, ggf. mit Bedingung
         //context.setState(context.getConcreteState2());
-        passenger.setState(passenger.getState());
+
+        //not sure if richtig: //passenger.setState(new Bronze());
     }
 
     @Override
     public void addStateListener(IStateListener listener) {
-
+        stateListeners.add(0,listener);
     }
 }
