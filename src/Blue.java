eb8d2c5c85f2e535f3a51c85b79f0bd3b8c36daf
andii04 <and.woerrlein.17@lehre.mosbach.dhbw.de>
2019-02-28 14:58:35 +0100

    Status Blue changed points

--- src/Blue.java
+++ src/Blue.java
@@ -3,13 +3,26 @@ public class Blue implements IState {
     int points;
     public void promote(Passenger passenger, int points) {
         //points.....;
-        if(points>=2000) {
-            passenger.setState(new Platinum());
+        this.points += points;
+        if(points>1999 && points <4250) {
+            passenger.setState(new Bronze());
         }
         //noch falsche Werte für Points
-        else if(points>=4000 && points <=8000)
+        else if(points>=4250 && points <6500)
+        {
+            passenger.setState(new Silver());
+        }
+        else if(points>=6500 && points <11500)
         {
             passenger.setState(new Gold());
         }
+        else if(points>=11500)
+        {
+            passenger.setState(new Platinum());
+        }
+        else {
+            System.out.println("Fehler");
+        }
+
     }
 }
