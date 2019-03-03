847e4aba6cad89221ec4b754a0433e7fc39c67bd
Wörrlein, Andreas <Andreas.Woerrlein@tecalliance.net>
2019-03-03 13:30:41 +0100

    repo 5000 Passenger

--- src/Application.java
+++ src/Application.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
-        Passenger p = new Passenger("Peter");
+        /*Passenger p = new Passenger("Peter");
         p.setState(new Blue());
         System.out.println(p);
         System.out.println();
         p.promote();
-        System.out.println(p);
+        System.out.println(p);*/
 
+        Passenger[] passengers = new Passenger[5000];
+        for(int i=0;i<passengers.length;i++)
+        {
+            passengers[i].setState(new Blue());
+            passengers[i].promote();
+        }
 
     }
 }
