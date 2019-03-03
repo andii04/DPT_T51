aa3fff19f3208307fbc6649674dbca8cdca296ae
Wörrlein, Andreas <Andreas.Woerrlein@tecalliance.net>
2019-03-03 12:08:59 +0100

    Train in Richtung Oberserver Pattern

--- src/Train.java
+++ src/Train.java
@@ -1,8 +1,24 @@
+import java.util.ArrayList;
+
 public class Train {
     int capacity = 500;
     SeatClass firstclass;
     SeatClass businessclass;
     SeatClass economy;
+
+    private ArrayList<ITrainListener> listeners;
+
+    public Train() {
+        listeners = new ArrayList<>();
+    }
+
+    public void addListener(ITrainListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeListener(ITrainListener listener) {
+        listeners.remove(listener);
+    }
 
 
 
